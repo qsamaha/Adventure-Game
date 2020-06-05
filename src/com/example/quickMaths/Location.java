d74f2d467d282c05e254b3fda780818ff8ff2fba
Qutada Samaha <qsamaha98@gmail.com>
2020-06-04 21:28:54 -0400

    First Commit

---
+++ src/com/example/quickMaths/Location.java
@@ -0,0 +1,30 @@
+package com.example.quickMaths;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+
+    private final int locationId;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationId, String description) {
+        this.locationId = locationId;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+        this.exits.put("Q", 0);
+    }
+
+    public void addExit(String direction, int location){
+        exits.put(direction, location);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}
